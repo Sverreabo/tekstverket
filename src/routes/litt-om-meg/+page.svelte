450fb448a520b10735c8f394e53d27994513d13b
yeePlayz <sverreabo@gmail.com>
2023-06-04 10:01:31 +0200

    04.06

--- src/routes/litt-om-meg/+page.svelte
+++ src/routes/litt-om-meg/+page.svelte
@@ -6,45 +6,98 @@
     <div class="container">
         <div class="flex-responsive">
             <div style="flex: 70%">
-                <h2>Litt om meg</h2>
-                <div class="bilde-liten" style="width: 40%;">
-                    <img
-                        src="/Anne Kunstforeningen.jpg"
-                        width="756"
-                        height="900"
-                    />
-                </div>
-                <p>
-                    Filolog og samfunnsviter. Har nesten 40 års ansiennitet fra
-                    Norges største forlag. Sveip innom organisasjonslivet,
-                    PR-byrå, presse, kringkasting og det offentlige
-                    omsorgsfeltet. Kan vise til spesielt god innsikt i kultur-
-                    og helsesektoren, både psykiatri og somatikk. Som forfatter
-                    har jeg utgitt ni bøker, og ellers arbeidet som
-                    PR-/reklamekonsulent, kommunikasjonsansvarlig,
-                    informasjonssjef, innholdsleverandør og nettjournalist. Kan
-                    vise til lang erfaring som taleskriver og
-                    sangtekstforfatter; fra gravferdstaler til humoristiske
-                    festtaler og -sanger. Lang erfaring som pårørende og verge,
-                    med bl.a. skriving av brev, søknader og klager til
-                    «hjelpeapparatet», fra NAV til spesialist- og
-                    kommunehelsetjenesten.
-                </p>
-                <h2>Kontaktinfo</h2>
-                <p>
-                    Anne Østgaard<br />
-                    E-post: <br />
-                    Mobil: 971 64 649
-                </p>
-                <h2>Betaling</h2>
-                <p>
-                    Timepris: Kr <br />
-                    Fastpris kan også avtales <br />
-                    Betaling kan gjøres via vipps: …… Eller nettbank, til kto.nr.:
-                </p>
+                <section>
+                    <h2>Litt om meg</h2>
+                    <div class="bilde-liten" style="width: 40%;">
+                        <img
+                            src="/Anne Kunstforeningen.jpg"
+                            width="756"
+                            height="900"
+                            alt="Anne"
+                        />
+                    </div>
+                    <p>
+                        Frilanser med 38 års fartstid som informasjons-
+                        /kommunikasjonsmedarbeider i Norges største forlag,
+                        Cappelen Damm. I tillegg bakgrunn fra PR-byrå,
+                        Dagbladet, NRK og organisasjonslivet. I tillegg
+                        forfatter med ti bøker på merittlisten. I hovedsak bøker
+                        for barn, som
+                        <i>
+                            <a
+                                href="https://cappelendamm.no/sok?query=anne%20%C3%B8stgaard"
+                                >Barske barn-serien</a
+                            ></i
+                        >. Men også voksenbøker som antologien
+                        <i>
+                            <a
+                                href="https://cappelendamm.no/_dodsdikt-9788202703158"
+                                >Dødsdikt</a
+                            ></i
+                        >. Har ellers erfaring som pårørende innenfor både
+                        psykiatri og somatikk, som arrangør av litterære
+                        samtaler, paneldebatter og pressekonferanser og fra
+                        flere styreverv. Gjennom livet arrangør av større og
+                        mindre fester, både privat og i jobbsammenheng, dert jeg
+                        også har skrevet og holdt utallige taler, forfattet
+                        festsanger og fest«tullegrammer». I de senere år oppdrag
+                        som autorisert gravferdstaler og vigsler i regi av
+                        HumanEtisk Forbund. Gjendiktninger som vinner av Nordisk
+                        Råds litteraturpris for barn og ungdom, Ulf Starks
+                        <i
+                            ><a
+                                href="https://cappelendamm.no/_dyr-som-ingen-sa-unntatt-vi-ulf-stark-9788202586119"
+                                >Dyr som ingen så unntatt vi</a
+                            ></i
+                        >
+                    </p>
+                    <p>
+                        Jeg har bred kompetanse på tekst, og behersker de fleste
+                        sjangere - med stø språkføring. Litteratur, kultur og
+                        helse er temaer jeg har et spesielt nært forhold til.
+                    </p>
+                </section>
+                <section>
+                    <h2>Brev, søknader, klager</h2>
+                    <p>
+                        Som pårørende til en psykisk syk bror og en alvorlig
+                        skadet sønn, har jeg har mange års erfaring med NAV,
+                        kommune- og spesialisthelstjenesten,
+                        forsikringsselskaper oa.. Jeg vet det er krevende å
+                        forholde seg til det såkalte «hjelpeapparatet»,
+                        orientere seg og manøvrere i jungelen av lover og
+                        forskrifter, og ulike instanser og nivåer. Trenger du
+                        råd og vink, eller hjelp til å skrive brev, søknader
+                        eller klager, bistår jeg gjerne.
+                    </p>
+                </section>
+                <section>
+                    <h2>Kontaktinfo</h2>
+                    <p>
+                        Anne Østgaard<br />
+                        E-post:
+                        <a href="mailto:anneostgaard@gmail.com"
+                            >anneostgaard@gmail.com</a
+                        ><br />
+                        Mobil: 971 64 649
+                    </p>
+                </section>
+                <section>
+                    <h2>Betaling</h2>
+                    <p>
+                        Timepris: Kr 750-1000<br />
+                        Fastpris kan også avtales <br />
+                        Betaling kan gjøres via vipps: …… Eller nettbank, til kto.nr.:
+                    </p>
+                </section>
             </div>
             <div style="flex: 30%" class="bilde-stor">
-                <img src="/Anne Kunstforeningen.jpg" width="756" height="900" />
+                <img
+                    src="/Anne Kunstforeningen.jpg"
+                    width="756"
+                    height="900"
+                    alt="Anne"
+                />
             </div>
         </div>
     </div>
---
+++ src/routes/oversettelser-og-gjendiktinger/+page.svelte
@@ -0,0 +1,22 @@
+<main>
+    <div class="container flex-responsive">
+        <div class="bilde-stor" style="flex: 40%;">
+            <img src="/Skrivemaskin.jpg" width="612" height="612" alt="" />
+        </div>
+        <div style="flex: 60%;">
+            <h2>Konsulenttjenester, oversettelser og gjendiktninger</h2>
+            <div class="bilde-liten">
+                <img src="/Skrivemaskin.jpg" width="612" height="612" alt="" />
+            </div>
+            <p>
+                Jeg påtar meg oversettelser og gjendiktninger fra dansk og
+                svensk. Gjerne bildebøker, som har et begrenset omfang. Viser i
+                denne sammenheng til min gjendiktning av Ulf Starks <i
+                    >Dyr som ingen så unntatt vi</i
+                >, vinner av Nordisk Råds litteraturpris for barn og ungdom
+                (Cappelen Damm 2018). I tillegg konsulenttjenester og språkvask
+                av barne- og ungdomslitteratur.
+            </p>
+        </div>
+    </div>
+</main>
