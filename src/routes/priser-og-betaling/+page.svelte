36ef7db78a61ce824bd9e4c7b570588b71a3abe9
yeePlayz <sverreabo@gmail.com>
2023-06-09 14:26:54 +0200

    09.06

--- src/routes/priser-og-betaling/+page.svelte
+++ src/routes/priser-og-betaling/+page.svelte
@@ -4,13 +4,19 @@
 
 <main>
     <div class="container flex-responsive">
-        <div>
+        <div style="flex: 60%;">
             <h2>Betaling</h2>
+            <div class="bilde-liten">
+                <img src="/betaling.jpg" width="1024" height="439" alt="" />
+            </div>
             <p>
                 Timepris: Kr 750-1000<br />
                 Fastpris kan også avtales <br />
                 Betaling kan gjøres via vipps: …… Eller nettbank, til kto.nr.:
             </p>
+        </div>
+        <div class="bilde-stor" style="flex: 40%;">
+            <img src="/betaling.jpg" width="1024" height="439" alt="" />
         </div>
     </div>
 </main>
